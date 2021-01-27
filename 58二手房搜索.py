a9de5b6e40d9a104435acc2f1a22eb86f74569da
ty21000 <ty21000@163.com>
2021-01-27 19:06:44 +0800

    更新解析方式

--- 58二手房搜索.py
+++ 58二手房搜索.py
@@ -15,11 +15,10 @@ def realTimeGet():
     # 列表 存所有房源信息
     all_list = []
     headers = {
-        "authority": "gl.58.com",
+        "host": "gl.58.com",
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "referer": "https://callback.58.com/",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-        "cookie": "userid360_xml=71AA4CEB68A4149CDA14A2A28361DF12; time_create=1613562377170; f=n; commontopbar_new_city_info=1039%7C%E6%A1%82%E6%9E%97%7Cgl; commontopbar_ipcity=gl%7C%E6%A1%82%E6%9E%97%7C0; 58home=gl; id58=c5/nfGAFdP1IXzr8BJLkAg==; city=gl; 58tj_uuid=e545b9d1-11bb-4eae-ba4f-08ed2b4e949f; als=0; wmda_uuid=0278ee87d687e0add91317d28f20f11e; wmda_new_uuid=1; wmda_visited_projects=%3B6333604277682%3B11187958619315; xxzl_deviceid=qQzkzbOJK%2FfZfLdxTidwCiUFsRYbhqaHRcRz3w3vSb8WgkiGqRfUpFGmOh0%2FsWP2; sessid=5FBF9842-2C06-45F5-B8EE-714DF935BA19; aQQ_ajkguid=84A89C36-4D1E-4003-8CC4-E15E0113819A; ctid=1039; JSESSIONID=463DDE22BB97A7C26C8DBAA920EBCEBC; wmda_session_id_6333604277682=1611113306655-823ff673-6baf-033c; new_session=1; new_uv=8; utm_source=; spm=; init_refer=https%253A%252F%252Fcallback.58.com%252F; wmda_session_id_11187958619315=1611113312138-188d32ba-370f-06e2; xzfzqtoken=IhGAKfoqkPWDCC7hxRnsdb2%2F45O%2FLKC%2BzZXr6PUP9zohqZGz7YzzaIbGJKWo8xRgin35brBb%2F%2FeSODvMgkQULA%3D%3D",
+        "cookie": "userid360_xml=71AA4CEB68A4149CDA14A2A28361DF12; time_create=1613562377170; f=n; commontopbar_new_city_info=1039%7C%E6%A1%82%E6%9E%97%7Cgl; 58home=gl; id58=c5/nfGAFdP1IXzr8BJLkAg==; city=gl; 58tj_uuid=e545b9d1-11bb-4eae-ba4f-08ed2b4e949f; als=0; wmda_uuid=0278ee87d687e0add91317d28f20f11e; wmda_new_uuid=1; wmda_visited_projects=%3B6333604277682%3B11187958619315; xxzl_deviceid=qQzkzbOJK%2FfZfLdxTidwCiUFsRYbhqaHRcRz3w3vSb8WgkiGqRfUpFGmOh0%2FsWP2; sessid=5FBF9842-2C06-45F5-B8EE-714DF935BA19; aQQ_ajkguid=84A89C36-4D1E-4003-8CC4-E15E0113819A; wmda_visited_projects=%3B6333604277682%3B11187958619315%3B8788302075828; ctid=1039; cid=1039; myfeet_tooltip=end; f=n; commontopbar_new_city_info=1039%7C%E6%A1%82%E6%9E%97%7Cgl; commontopbar_ipcity=nn%7C%E5%8D%97%E5%AE%81%7C0; new_uv=13; utm_source=; spm=; init_refer=https%253A%252F%252Fwww.baidu.com%252Flink%253Furl%253Ds9mZP0PgGyMy8WyhESC6YpXldaqNxrDHx7daxHCUoDi%2526wd%253D%2526eqid%253Db0260653000330dc00000006600fe34d; wmda_session_id_11187958619315=1611653974933-b7a64728-c475-8291; xxzl_cid=014738718b78413190e537639b0bdfe6; xzuid=2bb08420-5beb-44ba-9f9f-01188674cf79; new_session=0",
     }
     url = 'https://gl.58.com/ershoufang/?key={}'.format(parse.quote(keyword))
     page_text = requests.get(url=url, headers=headers).text
@@ -27,8 +26,8 @@ def realTimeGet():
     tree = etree.HTML(page_text)
     page_sum = 1
     try:
-        a_list = tree.xpath('//div[@class="pager"]/a')
-        page_sum_str = a_list[-2].xpath('./span/text()')[0]
+        a_list = tree.xpath('//li[@class="page-item last"]')
+        page_sum_str = a_list[0].xpath('./a/text()')[0]
         if page_sum_str!=None:
             page_sum = int(page_sum_str)
     except:
@@ -42,28 +41,33 @@ def realTimeGet():
             page_text = requests.get(url=url, headers=headers).text
 
         tree = etree.HTML(page_text)
-        li_list = tree.xpath('//ul[@class="house-list-wrap"]/li')
+        li_list = tree.xpath('//div[@class="property"]')
         for li in li_list:
-            temp_list = li.xpath('./div[2]/h2/a//text()')
-            base_info1 = li.xpath('./div[2]/p/span[1]/text()')[0]
-            base_info2 = li.xpath('./div[2]/p/span[2]/text()')[0]
+            #标题
+            temp_list = li.xpath('./a/div[2]/div[1]/div[1]/h3//text()')
+            #几房
+            base_info1 = ''.join(li.xpath('./a//p[@class="property-content-info-text property-content-info-attribute"]//text()'))
+            # 去除空格
+            base_info1 = "".join(base_info1.split())
+            base_info2 = li.xpath('./a//div[@class="property-content-info"]/p[2]/text()')[0]
             base_info = base_info1+','+base_info2
             #总价
-            price_sum = li.xpath('./div[3]/p[1]/b/text()')[0]
+            price_sum = li.xpath('./a//span[@class="property-price-total-num"]/text()')[0]
+            price_sum="".join(price_sum.split())
             #每平多少元
-            price_uint_str =li.xpath('./div[3]/p[2]/text()')[0]
+            price_uint_str =li.xpath('./a//p[@class="property-price-average"]/text()')[0]
             title = ''
             for e in temp_list:
                 title = title+e
 
             content = title+'('+base_info+')['+price_sum+'万,'+price_uint_str+']'
-            print(content)
             ex = '(\d{4,})'
             t4 = re.findall(ex,price_uint_str,re.S)
             price_uint= int(t4[0])
             #print(price_uint)
             #去除空格
             content = "".join(content.split())
+            print(content)
             #构造字典
             one_dict = {
                 'title':content,
@@ -74,7 +78,15 @@ def realTimeGet():
         time.sleep(3)
 
     if len(all_list) >0:
+        '''#去重
+        news_all_list = []
+        for id in all_list:
+            if id not in news_all_list:
+                news_all_list.append(id)
+        '''
+        #排序
         result_list = sorted(all_list,key=lambda x: (x['price_uint']))
+
         #保存到数据库
         st = SaveTool()
         st.savetodb(result_list,keyword=keyword)
